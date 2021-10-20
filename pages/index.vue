78a5a83ede254bb48b236c23260ae244298445f4
Joren Synaeve <synaevejoren@gmail.com>
2021-10-20 11:13:21 +0200

    Add first input

--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,39 @@
 <template>
-  <section class="section">
-    <div class="columns is-mobile">
-      <card
-        title="Free"
-        icon="github"
-      >
-        Open source on <a href="https://github.com/buefy/buefy">
-          GitHub
-        </a>
-      </card>
-
-      <card
-        title="Responsive"
-        icon="cellphone-link"
-      >
-        <b class="has-text-grey">
-          Every
-        </b> component is responsive
-      </card>
-
-      <card
-        title="Modern"
-        icon="alert-decagram"
-      >
-        Built with <a href="https://vuejs.org/">
-          Vue.js
-        </a> and <a href="http://bulma.io/">
-          Bulma
-        </a>
-      </card>
-
-      <card
-        title="Lightweight"
-        icon="arrange-bring-to-front"
-      >
-        No other internal dependency
-      </card>
+  <div class="section">
+    <div class="box content">
+      <h1 class="title">Test 1</h1>
+      <div class="columns">
+        <div class="column">
+          <b-field label="Input">
+            <b-input type="textarea" size="is-small"></b-input>
+          </b-field>
+          <b-field label="Output">
+            <b-input type="textarea"  size="is-small"></b-input>
+          </b-field>
+        </div>
+        <div class="column">
+          <b-field> <b-checkbox>Case insensitive</b-checkbox> </b-field>
+          <b-field> <b-checkbox>Ingore trailing whitespace</b-checkbox> </b-field>
+          <b-field> <b-checkbox>Ignore all whitespace</b-checkbox> </b-field>
+          <b-field> <b-checkbox>Substring</b-checkbox> </b-field>
+          <b-field> <b-checkbox>Regex</b-checkbox> </b-field>
+        </div>
+      </div>
     </div>
-  </section>
+    <div class="box content">
+      <h1 class="title">Test 2</h1>
+    </div>
+  </div>
 </template>
 
-<script>
-import Card from '~/components/Card'
+<script lang="ts">
+import { Vue, Component } from "nuxt-property-decorator";
 
-export default {
-  name: 'HomePage',
-
-  components: {
-    Card
-  }
-}
+@Component({
+  name: "Dashboard",
+  head: {
+    title: "Home",
+  },
+})
+export default class Index extends Vue {}
 </script>
